affa95f7c52d584b71de8f5a910e63ab7e03fb91
Sachin <32639496+sachintu47@users.noreply.github.com>
2026-08-19 19:32:12 +0530

    Remove junk chars and fix bump line
    Signed-off-by: sachintu47 <sachintu47@gmail.com>

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -2,11 +2,11 @@ node('linux') {
   stage ('Poll') {
     checkout([
       $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/nlohmann-json[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[N[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/nlohmann-jsonport.git']]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/nlohmann-json[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[N[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cport.git'),
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/nlohmann-jsonport.git'),
       string(name: 'PORT_DESCRIPTION', value: 'JSON for Modern C++'),
       string(name: 'BUILD_LINE', value: 'DEV')
     ]
